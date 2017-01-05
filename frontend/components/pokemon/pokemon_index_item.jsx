4b570b41c2a75e5fada920843da3c0dca54fa535
madrev <maddierevill@gmail.com>
2017-01-05 15:36:31 -0800

    add styling to pokemon index

--- frontend/components/pokemon/pokemon_index_item.jsx
+++ frontend/components/pokemon/pokemon_index_item.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router';
 
 const PokemonIndexItem = ({pokemon}) => (
   <li>
-    <Link to={`pokemon/${pokemon.id}`}>      
-      <span>{ pokemon.name }</span>
-      <img src={ pokemon.image_url } width="50px" />
+    <Link to={`pokemon/${pokemon.id}`}>
+      <span className='col-1-3'>{ pokemon.id }</span>
+      <img className='col-1-3' src={ pokemon.image_url } className='pokemon-list-image' />
+      <span className='col-1-3'>{ pokemon.name }</span>
     </Link>
   </li>
 );
